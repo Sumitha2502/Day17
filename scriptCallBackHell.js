6da02626380f3be7c4f0ccde358ef267c344771d
Sumitha2502 <sumitha0996@gmail.com>
2023-06-05 02:01:44 +0530

    Day16Task

---
+++ scriptCallBackHell.js
@@ -0,0 +1,39 @@
+var countDown = function() {
+    let button1 = document.querySelector(".btn1");
+    button1.setAttribute("style", "display: none");
+    let countdown = document.querySelector("#result");
+    let time = 10;
+    countdown.innerHTML = "";
+
+    countdown.innerHTML = time--;
+    setTimeout(() => {
+        countdown.innerHTML = time--;
+        setTimeout(() => {
+            countdown.innerHTML = time--;
+            setTimeout(() => {
+                countdown.innerHTML = time--;
+                setTimeout(() => {
+                    countdown.innerHTML = time--;
+                    setTimeout(() => {
+                        countdown.innerHTML = time--;
+                        setTimeout(() => {
+                            countdown.innerHTML = time--;
+                            setTimeout(() => {
+                                countdown.innerHTML = time--;
+                                setTimeout(() => {
+                                    countdown.innerHTML = time--;
+                                    setTimeout(() => {
+                                        countdown.innerHTML = time--;
+                                        setTimeout(() => {
+                                            countdown.innerHTML = "Happy Independence Day";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        }, 1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+};
